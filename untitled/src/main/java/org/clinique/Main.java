4b8c081f8d6cce81a55f7a032729808fd3b40550
Charles Lavoie <charles01_lv-2000@hotmail.com>
2025-09-15 19:29:33 -0400

    feat : main

---
+++ untitled/src/main/java/org/clinique/Main.java
@@ -0,0 +1,15 @@
+package org.clinique;
+
+import org.clinique.domain.TriageType;
+
+public class Main {
+    public static void main(String[] args) {
+        TriageType doctorTriageType = TriageType.FIFO;
+        TriageType radiologyTriageType = TriageType.FIFO;
+
+
+        // Clinic clinic = new Clinic(doctorTriageType, radiologyTriageType /* , ... */)
+        // clinic.triagePatient("John", 4, VisibleSymptom.MIGRAINE);
+    }
+
+}
